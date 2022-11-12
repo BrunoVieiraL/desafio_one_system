01385cf2a4ddf3b35e8d04163846297a6ad6bf35
Bruno Vieira <bruno.leo516@hotmail.com>
2022-11-12 10:18:36 -0300

    Mock of lists created

---
+++ lib/mock/list_mocks.dart
@@ -0,0 +1,9 @@
+import 'package:desafio_one_system/model/info_model.dart';
+
+class ListMocks {
+  static List<InfoModel> companyList = [];
+
+  static List<InfoModel> partnerList = [];
+
+  static List<InfoModel> procedureList = [];
+}
