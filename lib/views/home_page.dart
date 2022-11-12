5187314e8051328c11c820b1e7bfe4ccb95ea936
Bruno Vieira <bruno.leo516@hotmail.com>
2022-11-12 10:09:37 -0300

    Base project created

---
+++ lib/views/home_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF1C51AE),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Cabeçalho'),
+              Tab(text: 'Catálogo'),
+              Tab(text: 'Carrinho'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'Cabeçalho de Nota',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Color(0xFF2B518D)),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 16, left: 16),
+                  child: Text(
+                    'Preencha as informações de cabeçalho do pedido',
+                    style: TextStyle(
+                        color: Color(0xFF2B518D),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 90,
+                      height: 50,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.search),
+                    ),
+                    const SizedBox(
+                      width: 280,
+                      height: 50,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          label: Text(
+                            'Empresa',
+                            style: TextStyle(
+                                color: Color(0xFF2B518D),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const Center(
+              child: Text('Template'),
+            ),
+            const Center(
+              child: Text('Template'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
