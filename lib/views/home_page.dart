fb3be06bba62d0dd1d871089cee12f4cf4f9f24a
Bruno Vieira <bruno.leo516@hotmail.com>
2022-11-13 15:01:50 -0300

    widget refactor initialized

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,9 @@
-import 'package:desafio_one_system/controllers/home_controller.dart';
+import 'package:desafio_one_system/controllers/company_controller.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'widgets/double_textfield_row_custom_widget.dart';
 
 // ignore: must_be_immutable
 class HomePage extends StatelessWidget {
@@ -10,7 +12,7 @@ class HomePage extends StatelessWidget {
   TextEditingController companyController = TextEditingController();
   TextEditingController idController = TextEditingController();
   TextEditingController searchController = TextEditingController();
-  final controller = Get.put(HomeController());
+  final controller = Get.put(CompanyController());
 
   @override
   Widget build(BuildContext context) {
@@ -55,73 +57,11 @@ class HomePage extends StatelessWidget {
                         fontWeight: FontWeight.bold),
                   ),
                 ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 16),
-                      child: TextField(
-                        controller: idController,
-                        onChanged: (value) {
-                          controller.autoComplete(value, companyController);
-                        },
-                        decoration: const InputDecoration(
-                          constraints: BoxConstraints(maxWidth: 80),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: IconButton(
-                        padding: EdgeInsets.zero,
-                        onPressed: () {
-                          dialogList();
-                        },
-                        icon: const Icon(
-                          Icons.search,
-                          size: 32,
-                        ),
-                      ),
-                    ),
-                    TextField(
-                      onTap: () {
-                        dialogList();
-                      },
-                      controller: companyController,
-                      decoration: const InputDecoration(
-                        constraints: BoxConstraints(maxWidth: 245),
-                        label: Text(
-                          'Empresa',
-                          style: TextStyle(
-                              color: Color(0xFF2B518D),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
-                        ),
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(10),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(right: 12),
-                      child: Text(
-                        '*',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+                DoubleTextFieldRowCustomWidget(
+                    idController: idController,
+                    controller: controller,
+                    listController: companyController,
+                    searchController: searchController),
               ],
             ),
             const Center(
@@ -129,53 +69,6 @@ class HomePage extends StatelessWidget {
             ),
             const Center(
               child: Text('Template'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future<dynamic> dialogList() {
-    return Get.defaultDialog(
-      title: ''.trim(),
-      titlePadding: EdgeInsets.zero,
-      content: SizedBox(
-        height: 400,
-        width: 300,
-        child: Column(
-          children: [
-            TextField(
-              keyboardType: TextInputType.number,
-              controller: searchController,
-              onChanged: (value) {
-                return controller.filterList(value);
-              },
-            ),
-            const SizedBox(height: 15),
-            Expanded(
-              child: Obx(
-                () => ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: controller.foundCompanies.value.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(
-                          '${controller.foundCompanies.value[index].id} - ${controller.foundCompanies.value[index].name}'),
-                      onTap: () {
-                        idController.text = controller
-                            .foundCompanies.value[index].id
-                            .toString();
-                        companyController.text =
-                            controller.foundCompanies.value[index].name;
-                        searchController.clear();
-                        controller.onInit();
-                        Get.back();
-                      },
-                    );
-                  },
-                ),
-              ),
             ),
           ],
         ),
